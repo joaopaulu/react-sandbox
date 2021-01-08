6ab8d22f9d96742bc9e81b51f1e830d75cf83efe
João Paulo <jptick@gmail.com>
2021-01-07 21:23:24 -0300

    Style Components and Refatoring Structure Folders

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,13 +1,17 @@
-import FormularioCadastro from './components/FormularioCadastro';
+import React, { Component } from 'react';
 import ListaDeNotas from './components/ListaDeNotas';
-
-function App() {
-  return (
-    <section>
-      <FormularioCadastro />
-      <ListaDeNotas />
-    </section>
-  );
+import FormularioCadastro from './components/FormularioCadastro';
+import './assets/App.css';
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
