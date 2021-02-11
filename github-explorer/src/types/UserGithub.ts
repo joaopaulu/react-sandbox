2129f45129b133297b706765f64639e160561a4d
João Paulo <jptick@gmail.com>
2021-02-11 08:37:39 -0300

    Teste React Typescript with Github API

---
+++ github-explorer/src/types/UserGithub.ts
@@ -0,0 +1,12 @@
+export type UserGithub = {
+  id: number;
+  login: string;
+  avatar_url: string;
+  url: string;
+  name: string;
+  company: string;
+  location: string;
+  blog: string;
+  public_repos: number;
+  html_url: string;
+};
